426422942b20db275aabcc5e9b401e835a145cf1
Imran00852 <bhatimran9797@gmail.com>
2024-05-05 17:14:31 +0530

    uploaded

---
+++ src/components/Home.tsx
@@ -0,0 +1,56 @@
+import { Button, Container, Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const selectLanguageHandler = (language: string): void => {
+    navigate(`/learn?language=${language}`);
+  };
+  const languages = [
+    {
+      name: "Japanese",
+      code: "ja",
+    },
+    {
+      name: "Hindi",
+      code: "hi",
+    },
+    {
+      name: "Spanish",
+      code: "es",
+    },
+    {
+      name: "French",
+      code: "fr",
+    },
+  ];
+  return (
+    <Container maxWidth={"sm"}>
+      <Typography variant="h3" p={"2rem"} textAlign={"center"}>
+        Welcome
+      </Typography>
+      <Stack
+        direction={"row"}
+        spacing={"2rem"}
+        p={"2rem"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        {languages.map((i) => (
+          <Button
+            onClick={() => selectLanguageHandler(i.code)}
+            key={i.code}
+            variant="contained"
+          >
+            {i.name}
+          </Button>
+        ))}
+      </Stack>
+      <Typography textAlign={"center"}>
+        Choose one language from above
+      </Typography>
+    </Container>
+  );
+};
+
+export default Home;
